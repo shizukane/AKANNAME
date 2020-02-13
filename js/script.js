219eec86f248b51be24b9001914fe67bd228bef7
shizukane <drizydarolle@gmail.com>
2020-02-13 12:33:22 +0300

    added text to js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,16 @@ var femaleNames=[
     "Afua",
     "Ama",
 ];
+//calling functions
+function getName() {
+var year=parseInt(document.getElementById("year").value);
+var month =parseInt(document.getElementById("month").value);
+var day=parseInt(document.getElementById("day").value);
+var male = document.getElementById("male");
+var female = document.getElementById("female");
+//validation
+if(day<=0||day>31)
+alert("Please enter the correct day");
+else if(month<0||month>12 ||month==2 &&day>29)
+alert("Please enter the correct day");
+
